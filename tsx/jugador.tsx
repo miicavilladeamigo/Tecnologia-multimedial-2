f442e3f5a079121b8d85bb490d1a9617378d0d41
miicavilladeamigo <66319880+miicavilladeamigo@users.noreply.github.com>
2021-08-10 16:14:56 -0300

    Add files via upload

---
+++ tsx/jugador.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="jugador" tilewidth="32" tileheight="32" tilecount="361" columns="19">
+ <image source="../jugador.png" width="610" height="610"/>
+</tileset>
